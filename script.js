fa7e134c2e15e19ab1d81fe820add20d2e96b448
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-10-09 14:22:00 +0500

    start javascript and add onclick function on button.

---
+++ script.js
@@ -0,0 +1,5 @@
+function calculateBMI(){
+    var height=document.getElementById("height").value
+    var weight=document.getElementById("weight").value
+    var result = document.getElementById("result")
+}
