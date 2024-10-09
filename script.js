c868d1412f7384a3ead88d21b5f018e928f3b17f
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-10-09 14:42:07 +0500

    calculate the bmi.

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ function calculateBMI(){
     var height=document.getElementById("height").value
     var weight=document.getElementById("weight").value
     var result = document.getElementById("result")
+
+    //  BMI Formula
+    let bmi = weight / (height / 100 * height / 100)
+    let totalBmi = bmi.toFixed(2);
+    result.textContent = "Your BMI is :" + totalBmi
 }
